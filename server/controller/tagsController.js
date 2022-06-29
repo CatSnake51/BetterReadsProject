703694747cbf27b08fce7c3e7fd05743c4666e49
griffin104 <griffinbrlw@gmail.com>
2022-06-29 18:25:15 -0400

    WIP

---
+++ server/controller/tagsController.js
@@ -0,0 +1,11 @@
+const db = require('../models/betterReadsModel.js')
+
+const tagsController = {}
+
+tagsController.getTags = async (req, res, next) => {
+  try {
+    const 
+  } catch (err) {
+    return next(err)
+  }
+}
