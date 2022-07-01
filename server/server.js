1813c535d00cdb5e2c9a54609187945785b797de
Katherine <80862932+katherinek123@users.noreply.github.com>
2022-06-30 18:34:07 -0700

    fixed test error

--- server/server.js
+++ server/server.js
@@ -9,33 +9,33 @@ const port = 3000;
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/reviews', reviewsRouter); 
+app.use('/api/reviews', reviewsRouter);
 app.use('/api/tags', tagsRouter)
 
 // send index.html file to base endpoint
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'))
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
 
 
 app.use((req, res) => {
-    res.status(404).json("Page not found");
+  res.status(404).json("Page not found");
 })
 
 app.use((err, req, res, next) => {
-    const defaultError = {
-        log: "Error in unknown middleware",
-        status: 400,
-        message: {err: "Error sent in response"}
-    }
-    const errorObj = Object.assign(defaultError, err);
-    console.log(errorObj.log);
-    return res.status(errorObj.status).json(errorObj.message)
+  const defaultError = {
+    log: "Error in unknown middleware",
+    status: 400,
+    message: { err: "Error sent in response" }
+  }
+  const errorObj = Object.assign(defaultError, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message)
 })
 
 app.listen(port, () => {
